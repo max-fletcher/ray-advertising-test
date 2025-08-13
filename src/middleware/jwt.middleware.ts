0f6945237553a05404d8747cad00c6015d56c5fe
Mahin Chowdhury <amar.naam.machine@gmail.com>
2025-08-13 16:24:22 +0600

    Build successful

--- src/middleware/jwt.middleware.ts
+++ src/middleware/jwt.middleware.ts
@@ -1,61 +1,12 @@
 import { NextFunction, Response } from 'express';
 import { sign, TokenExpiredError, verify } from 'jsonwebtoken';
-import { AdminUserRepository } from '../db/rdb/repositories/admin-user.repository';
-import { AppUserPayload, UserPayload } from '../schema/token-payload.schema';
-import {
-  IAuthenticatedRequest,
-  IAuthenticatedRequest,
-} from '../types/interfaces/authenticate.interface';
+import { AppUserPayload } from '../schema/token-payload.schema';
+import { IAuthenticatedRequest } from '../types/interfaces/authenticate.interface';
 import { getEnvVar } from '../utils/common.utils';
 import { AppUserRepository } from '../db/rdb/repositories/app-user.repository';
 import { datetimeYMDHis } from '../utils/datetime.utils';
 
 export class JwtMiddleware {
-  generateToken(payload: UserPayload): string {
-    const expiresIn = Number(getEnvVar('JWT_EXPIRY'));
-    return sign(payload, getEnvVar('JWT_SECRET'), {
-      expiresIn: `${expiresIn} days`,
-    });
-  }
-
-  async verifyToken(
-    req: IAuthenticatedRequest,
-    res: Response,
-    next: NextFunction,
-  ) {
-    try {
-      const authHeader = req.headers.authorization;
-
-      if (!authHeader || !authHeader?.startsWith('Bearer '))
-        return res.status(401).json({ message: 'You are not logged in!' });
-
-      const token = authHeader.split(' ')[1];
-
-      const payload = verify(token, getEnvVar('JWT_SECRET'));
-
-      if (payload) {
-        req.user = payload as UserPayload;
-        const adminRepo = new AdminUserRepository();
-        const checkUser = await adminRepo.findUserById(req.user.id);
-
-        if (!checkUser) {
-          throw new TokenExpiredError('Invalid Token Data', new Date());
-        }
-
-        next();
-      } else {
-        throw new TokenExpiredError('Token Expired', new Date());
-      }
-    } catch (e: any) {
-      if (e instanceof TokenExpiredError)
-        return res.status(401).json({ message: e.message });
-
-      return res
-        .status(500)
-        .json({ message: 'Something went wrong! Please try again.' });
-    }
-  }
-
   generateAppUserToken(payload: AppUserPayload): {
     token: string;
     validity: string;
@@ -87,9 +38,9 @@ export class JwtMiddleware {
       if (payload) {
         req.user = payload as AppUserPayload;
         const userRepo = new AppUserRepository();
-        const checkUser = await userRepo.userExistsById(req.user.id);
+        const checkUser = await userRepo.findById(req.user.id);
 
-        if (checkUser === 0) {
+        if (!checkUser) {
           throw new TokenExpiredError('Invalid token provided!', new Date());
         }
 
@@ -126,9 +77,9 @@ export class JwtMiddleware {
           req.user = payload as AppUserPayload;
           if(!payload.device_id){
             const userRepo = new AppUserRepository();
-            const checkUser = await userRepo.userExistsById(req.user.id);
+            const checkUser = await userRepo.findById(req.user.id);
   
-            if (checkUser === 0)
+            if (!checkUser)
               throw new TokenExpiredError('Invalid token provided!', new Date());
   
             next();
