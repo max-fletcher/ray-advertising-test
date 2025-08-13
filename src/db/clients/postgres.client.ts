5d6c3ef08b0e350d57589bfd21111cd680da9d5b
Mahin Chowdhury <amar.naam.machine@gmail.com>
2025-08-13 16:03:18 +0600

    Restoted postgres client for sequelize

--- src/db/clients/postgres.client.ts
+++ src/db/clients/postgres.client.ts
@@ -1,27 +1,26 @@
-// import { Sequelize } from 'sequelize';
-// import { getEnvVar } from '../../utils/common.utils';
-// import { getDBClient } from '../../utils/db-client.utils';
+import { Sequelize } from 'sequelize';
+import { getEnvVar } from '../../utils/common.utils';
+import { getDBClient } from '../../utils/db-client.utils';
 
-// export class UserClient {
-//   private static instance: Sequelize | null = null;
-//   private static dbName = getEnvVar('USER_DB_NAME');
-//   private static username = getEnvVar('USER_DB_USERNAME');
-//   private static password = getEnvVar('USER_DB_PASSWORD');
-//   private static endpoint = getEnvVar('USER_DB_ENDPOINT');
-//   private static port = getEnvVar('USER_DB_PORT');
+export class UserClient {
+  private static instance: Sequelize | null = null;
+  private static dbName = getEnvVar('USER_DB_NAME');
+  private static username = getEnvVar('USER_DB_USERNAME');
+  private static password = getEnvVar('USER_DB_PASSWORD');
+  private static endpoint = getEnvVar('USER_DB_ENDPOINT');
+  private static port = getEnvVar('USER_DB_PORT');
+  constructor() {}
 
-//   constructor() {}
-
-//   public static getInstance(): Sequelize {
-//     if (!UserClient.instance) {
-//       UserClient.instance = getDBClient(
-//         UserClient.dbName,
-//         UserClient.username,
-//         UserClient.password,
-//         UserClient.endpoint,
-//         UserClient.port,
-//       );
-//     }
-//     return UserClient.instance;
-//   }
-// }
+  public static getInstance(): Sequelize {
+    if (!UserClient.instance) {
+      UserClient.instance = getDBClient(
+        UserClient.dbName,
+        UserClient.username,
+        UserClient.password,
+        UserClient.endpoint,
+        UserClient.port,
+      );
+    }
+    return UserClient.instance;
+  }
+}
--- src/db/rdb/models/app-user.model.ts
+++ src/db/rdb/models/app-user.model.ts
@@ -1,65 +1,65 @@
-// import { UserClient } from '../../clients/postgres.client';
-// import {
-//   Model,
-//   DataTypes,
-//   InferAttributes,
-//   InferCreationAttributes,
-//   CreationOptional,
-// } from 'sequelize';
+import { UserClient } from '../../clients/postgres.client';
+import {
+  Model,
+  DataTypes,
+  InferAttributes,
+  InferCreationAttributes,
+  CreationOptional,
+} from 'sequelize';
 
-// const sequelize = UserClient.getInstance();
+const sequelize = UserClient.getInstance();
 
-// class AppUserModel extends Model<
-//   InferAttributes<AppUserModel>,
-//   InferCreationAttributes<AppUserModel>
-// > {
-//   declare id: string;
-//   declare email: CreationOptional<string>;
-//   declare password: CreationOptional<string>;
-//   declare firstName: CreationOptional<string | null>;
-//   declare lastName: CreationOptional<string | null>;
-//   declare avatarUrl: CreationOptional<string | null>;
-//   declare deletedAt: CreationOptional<string | null>;
-//   declare deletedBy: CreationOptional<string | null>;
-// }
+class AppUserModel extends Model<
+  InferAttributes<AppUserModel>,
+  InferCreationAttributes<AppUserModel>
+> {
+  declare id: string;
+  declare email: CreationOptional<string>;
+  declare password: CreationOptional<string>;
+  declare firstName: CreationOptional<string | null>;
+  declare lastName: CreationOptional<string | null>;
+  declare avatarUrl: CreationOptional<string | null>;
+  declare deletedAt: CreationOptional<string | null>;
+  declare deletedBy: CreationOptional<string | null>;
+}
 
-// AppUserModel.init(
-//   {
-//     id: {
-//       type: DataTypes.STRING,
-//       primaryKey: true,
-//     },
-//     email: {
-//       type: DataTypes.STRING
-//     },
-//     password: {
-//       type: DataTypes.STRING
-//     },
-//     firstName: {
-//       type: DataTypes.STRING,
-//       defaultValue: null,
-//     },
-//     lastName: {
-//       type: DataTypes.STRING,
-//       defaultValue: null,
-//     },
-//     avatarUrl: {
-//       type: DataTypes.STRING,
-//     },
-//     deletedAt: {
-//       type: DataTypes.DATE,
-//       defaultValue: null,
-//     },
-//     deletedBy: {
-//       type: DataTypes.STRING,
-//       defaultValue: null,
-//     },
-//   },
-//   {
-//     tableName: 'app_users',
-//     sequelize,
-//     timestamps: true,
-//   },
-// );
+AppUserModel.init(
+  {
+    id: {
+      type: DataTypes.STRING,
+      primaryKey: true,
+    },
+    email: {
+      type: DataTypes.STRING
+    },
+    password: {
+      type: DataTypes.STRING
+    },
+    firstName: {
+      type: DataTypes.STRING,
+      defaultValue: null,
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      defaultValue: null,
+    },
+    avatarUrl: {
+      type: DataTypes.STRING,
+    },
+    deletedAt: {
+      type: DataTypes.DATE,
+      defaultValue: null,
+    },
+    deletedBy: {
+      type: DataTypes.STRING,
+      defaultValue: null,
+    },
+  },
+  {
+    tableName: 'app_users',
+    sequelize,
+    timestamps: true,
+  },
+);
 
-// export { AppUserModel };
+export { AppUserModel };
